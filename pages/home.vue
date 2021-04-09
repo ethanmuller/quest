736be4b96b1d690e873c5fb22009a7c32a797517
Ethan Muller <hi@emu.media>
2021-04-09 12:37:45 -0700

    start building interface

--- pages/home.vue
+++ pages/home.vue
@@ -1,7 +1,20 @@
 <template>
-  <main>
-    <div>home</div>
-    <NuxtLink to="/joining">joining</NuxtLink>
+<main>
+  
+  <div class="join-chunk">
+    <div class="input-and-button">
+      <label>
+        Lobby Code
+        <input type="text">
+      </label>
+      <NuxtLink class="join-button" to="/joining">Join</NuxtLink>
+    </div>
+  </div>
+  
+    <p class="host-note">
+      First one here? Maybe you want to
+      <NuxtLink to="/joining">Create Room</NuxtLink>
+    </p>
   </main>
 </template>
 
@@ -9,4 +22,38 @@
 </script>
 
 <style>
+  main {
+    min-height: calc(100vh - 5rem);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .join-chunk {
+    flex: 1;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .input-and-button {
+    display: flex;
+    align-items: flex-end;
+  }
+
+  .join-button {
+  display: inline-block;
+  padding: 0.5rem 2rem;
+  }
+
+  .host-note {
+    align-self: end;
+    padding-right: 3rem;
+  }
+
+  input {
+    display: block;
+  }
 </style>
